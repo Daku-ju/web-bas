c601e36ccec3bf43bde5af9ca1587bd75bcf32d9
LauraKeerman <95253457+LauraKeerman@users.noreply.github.com>
2022-04-30 03:50:04 +0200

    header page collections

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,21 @@
-console.info('Hello world');
+// Burger Menu
+let btn = document.querySelector("#burger-button");
+btn.addEventListener("click", menu);
+let burger = document.querySelector("#burger-menu");
+let display = "off";
+function menu(){
+    if (display == "off"){
+        burger.classList.remove("header--no-display");
+        setTimeout(function(){
+            burger.classList.remove("header--display");
+            btn.classList.add("header--cross");
+        },1);
+        display = "on";
+    }else if (display == "on"){
+        burger.classList.add("header--display");
+        btn.classList.remove("header--cross");
+        setTimeout(function(){burger.classList.add("header--no-display");},300);
+        display = "off";
+    }
+}
+
